63847911fc03245410a8408649a7fb98909fc937
milesangelo <milesvendetti@gmail.com>
2025-06-26 23:50:13 -0400

    chore(workflows): update debug output for Client ID to be base64 encoded

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "azurerm" {
-    #TODO: should we use a different resource group?
-    #TODO: does this need to be environment specific?
     resource_group_name  = "rg-kilometers-terraform"
     storage_account_name = "stkilometerstfstate"
     container_name       = "tfstate"
